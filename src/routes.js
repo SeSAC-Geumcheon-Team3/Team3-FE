2f47e71b3de653c7461a012a4455cb6f1ab43ed8
midofsummer <midofsummerr@gmail.com>
2024-09-03 14:42:12 +0900

    z

--- src/routes.js
+++ src/routes.js
@@ -1,62 +1,69 @@
-// src/routes.js
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.4
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
-import Signup from "views/examples/Signup";
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
-import Tables from "views/examples/Tables.js";
+import Signup from "views/examples/Signup";
 import Icons from "views/examples/Icons.js";
 
-const routes = [
+var routes = [
   {
     path: "/index",
     name: "Dashboard",
     icon: "ni ni-tv-2 text-primary",
-    component: Index,
+    component: <Index />,
     layout: "/admin",
   },
   {
     path: "/icons",
     name: "Icons",
     icon: "ni ni-planet text-blue",
-    component: Icons,
-    layout: "/admin",
-  },
-  {
-    path: "/signup",
-    name: "Signup",
-    icon: "ni ni-pin-3 text-orange",
-    component: Signup,
+    component: <Icons />,
     layout: "/admin",
   },
   {
     path: "/user-profile",
     name: "User Profile",
     icon: "ni ni-single-02 text-yellow",
-    component: Profile,
+    component: <Profile />,
     layout: "/admin",
   },
   {
-    path: "/tables",
-    name: "Tables",
+    path: "/Signup",
+    name: "Signup",
     icon: "ni ni-bullet-list-67 text-red",
-    component: Tables,
+    component: <Signup />,
     layout: "/admin",
   },
   {
     path: "/login",
     name: "Login",
     icon: "ni ni-key-25 text-info",
-    component: Login,
+    component: <Login />,
     layout: "/auth",
   },
   {
     path: "/register",
     name: "Register",
     icon: "ni ni-circle-08 text-pink",
-    component: Register,
+    component: <Register />,
     layout: "/auth",
   },
 ];
-
 export default routes;
